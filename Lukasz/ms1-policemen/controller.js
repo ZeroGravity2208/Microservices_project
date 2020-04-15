1911c6d24c07656d54a8f8c8694dbc66f46f82a2
Lukasz Adamczyk <lukasz012798@gmail.com>
2020-04-15 21:37:34 +0200

    15 kwietnia

--- Lukasz/ms1-policemen/controller.js
+++ Lukasz/ms1-policemen/controller.js
@@ -1,3 +1,56 @@
-const checkId = id => {
+const service = require('./service');
 
+const getPoliceman = (callback, id) => {
+  if (typeof (id) !== 'string' || id.length !== 24) {
+    callback({
+      error: 'Podałeś niepoprawny identyfikator'
+    })
+    return;
+  }
+  service.getPoliceman(data => callback(data), id)
 }
+
+const postPoliceman = (callback, body) => {
+  const {
+    imie,
+    nazwisko,
+    nr_identyfikacyjny,
+    nr_legitymacji,
+    stopien
+  } = body;
+  let isReturn = 0;
+  let myError = {};
+  // if (!imie || !nazwisko || !nr_identyfikacyjny || !nr_legitymacji || !stopien) {
+  //   callback('Nie podałeś wszystkich danych!');
+  //   return;
+  // };
+
+  // if (imie.length < 3) {
+  //   myError.errName = 'Imie musi mieć co najmniej 3 znaki!'
+  //   isReturn = 1;
+  // }
+
+  // if (nazwisko.length < 2) {
+  //   myError.errSurname = 'Nazwisko musi mieć co najmniej 2 znaki!'
+  //   isReturn = 1;
+  // }
+
+  if (!Number(nr_identyfikacyjny)) {
+    myError.errIdNo_1 = 'Nr identyfikacyjny jest ciągiem cyfr!';
+    isReturn = 1;
+  }
+
+  if (nr_identyfikacyjny.length !== 6) {
+    myError.errIdNo_2 = 'Nr identyfikacyjny jest sześciocyfrowy!';
+    isReturn = 1;
+  }
+
+  if (isReturn) {
+    callback(myError)
+    return
+  }
+  callback('okej');
+}
+
+module.exports.getPoliceman = getPoliceman;
+module.exports.postPoliceman = postPoliceman;
--- Lukasz/ms1-policemen/routes/api.js
+++ Lukasz/ms1-policemen/routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // const Policemen = require('./../models/policemen');
 const DAL = require('./../DAL');
 const service = require('./../service');
+const controller = require('./../controller');
 
 /* GET home page. */
 router.get('/policemen', (req, res) => {
@@ -11,14 +12,19 @@ router.get('/policemen', (req, res) => {
 
 router.get('/policeman/get/:id', (req, res) => {
   // DAL.getPoliceman(data => res.json(data), req.params.id);
-  service.getPoliceman(data => res.json(data), req.params.id);
+  // service.getPoliceman(data => res.json(data), req.params.id);
+  controller.getPoliceman(data => res.json(data), req.params.id);
 });
 
-router.get('/policemen/add', (req, res) => {
+router.get('/policeman/add', (req, res) => {
   res.render('policemen-form', {
     title: 'Dodaj policjanta',
   });
 });
+
+router.post('/policeman/add', (req, res) => {
+  controller.postPoliceman(data => res.json(data), req.body)
+})
 
 router.get('/policeman/update', (req, res) => {
   DAL.patchPoliceman(data => res.json(data), req.body);
